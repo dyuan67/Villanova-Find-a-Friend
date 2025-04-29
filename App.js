09e30ac1646a80662b6ea8b6b870625a0f0ad4ee
dyuan67 <dyuan@villanova.edu>
2025-04-29 18:39:40 -0400

    Added possible fonts to use and introduced to Welcome Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './src/navigation/AppNavigation';
+import QuestionnaireScreen from './src/screens/QuestionnaireScreen';
 
 export default function App() {
   return (
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,9 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
 
 export default function WelcomeScreen() {
+    const [fontsLoaded, fontError] = useFonts({
+        SpaceGroteskSemiBold: require("../font/SpaceGrotesk-SemiBold.ttf"),
+        SpaceGroteskBold: require("../font/SpaceGrotesk-Bold.ttf"),
+        SpaceGroteskMedium: require("../font/SpaceGrotesk-Medium.ttf"),
+      });
+
+      const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded || fontError) {
+            await SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded, fontError]);
+
+      if (!fontsLoaded) {
+        return null;
+      }
+
     return (
-        <View>
+        <View onLayout = {onLayoutRootView}>
             <Text>WelcomeScreen</Text>
         </View>
     )
