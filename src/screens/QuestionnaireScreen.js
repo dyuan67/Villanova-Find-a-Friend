297e9b2397a066f037b65116f014e2d0d47efc27
dyuan67 <dyuan@villanova.edu>
2025-04-29 16:59:36 -0400

    Created some of the neccessaey screens: profile, questionnaire, home

---
+++ src/screens/QuestionnaireScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function QuestionnaireScreen() {
+    return (
+        <View>
+            <Text>QuestionnaireScreen</Text>
+        </View>
+    )
+}
