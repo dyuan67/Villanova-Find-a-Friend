add03b6b17e71b5980cb9c411f5a45adbc1a11c1
dyuan67 <dyuan@villanova.edu>
2025-04-29 22:19:45 -0400

    Adding HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import {profile} from "../../assets/images"
 
 export default function HomeScreen() {
     return (
-        <View>
+        <SafeAreaView 
+        edges = {['top', 'left', 'right']}
+        className = "bg-white flex-1 justify-between"
+        style = {{
+            paddingTop: hp(2)
+        }}
+        >
+            {/* Header */}
+            <View className = "w-full flex-row justify-between items-center px-4 mb-8">
+                {/* Image */}
+                <View classname = "rounded-fill items-center justify-center"></View>
+                <Image 
+                    source = {profile}
+                    style = {{
+                        width: hp(4.5),
+                        height: hp(4.5),
+                        resizeMode: "cover",
+                        borderRadius: 50
+                    }}
+                    className = "rounded-full"
+                />
+            </View>
+
             <Text>HomeScreen</Text>
-        </View>
-    )
+        </SafeAreaView>
+    );
 }
