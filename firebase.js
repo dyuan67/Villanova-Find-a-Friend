32ac71e6a0b3c9ea9da2b43be0471a028fe2a30b
leyton <leytonxy21@gmail.com>
2025-04-29 22:03:40 -0400

    Added firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYss9qGYLIVbKH749UIqmgkGVT9aqwK0s",
+  authDomain: "csc-2053-find-a-friend.firebaseapp.com",
+  projectId: "csc-2053-find-a-friend",
+  storageBucket: "csc-2053-find-a-friend.firebasestorage.app",
+  messagingSenderId: "894407990666",
+  appId: "1:894407990666:web:af9985872f3ce135c1bc73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
