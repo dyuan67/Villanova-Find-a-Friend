fb90374586d88d01fd1d0a957609862bf6cfd7b5
dyuan67 <dyuan@villanova.edu>
2025-04-29 21:24:26 -0400

    Added the icons to the bottom tab navigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import WelcomeScreen from "../screens/WelcomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import QuestionnaireScreen from "../screens/QuestionnaireScreen";
 import MatchesScreen from "../screens/MatchesScreen";
 import HomeScreen from "../screens/HomeScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 
 const Stack = createNativeStackNavigator();
@@ -18,10 +18,43 @@ export default function AppNavigation() {
 
     const HomeTabs = () => {
         return (
-            <Tab.Navigator>
+            <Tab.Navigator 
+                screenOptions={({route}) => ({
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        let iconName;
+                        if (route.name === "Home") {
+                            iconName = "home";
+                          } else if (route.name === "Matches") {
+                            iconName = "happy-outline";
+                          } else if (route.name === "Profile") {
+                            iconName = "person-outline";
+                          }
+              
+                          const customizeSize = 25;
+                        
+                          return (
+                            <Ionicons
+                                name={iconName}
+                                size={customizeSize}
+                                color={focused ? "#3B82F6" : "gray"}
+                            />
+                          );
+                    },
+
+                    tabBarActiveTintColor: "#3B82F6",
+                    tabBarLabelStyle: {
+                        fontWeight: "bold",
+                    },
+                    tabBarInactiveTintColor: "gray",
+                    // tabBarShowLabel: false,
+                    tabBarStyle: {
+                     },
+                })}
+            >
                 <Tab.Screen name = "Home" component = {HomeScreen} />
                 <Tab.Screen name = "Profile" component = {ProfileScreen} />
-                <Tab.Screen name = "Matches" component = {QuestionnaireScreen} />
+                <Tab.Screen name = "Matches" component = {MatchesScreen} />
             </Tab.Navigator>
         );
     };
